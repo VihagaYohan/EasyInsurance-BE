5fe42934cfe4e05568148152ab65703a18e6260c
Vihaga Yohan <vihagayohan94@gmail.com>
2022-01-16 00:21:02 +0530

    Added additional error validations

--- controllers/InsuranceTypes.js
+++ controllers/InsuranceTypes.js
@@ -12,101 +12,104 @@ exports.getInsuranceTypes = async (req, res, next) => {
       data: insuranceTypes,
     });
   } catch (error) {
-   next(error)
-};
-
-// @desc    Get a Insurance type
-// @route   GET /api/v1/insuranceTypes/:id
-// @access  Public
-exports.getInsuranceType = async (req, res, next) => {
-  try {
-    const insuranceType = await InsuranceType.findById(req.params.id);
-    if (!insuranceType) {
-      return next(
-        new ErrorResponse(
-          `Unable to locate insurance type for ${req.params.id}`,
-          404
-        )
-      );
-    }
-    res.status(200).json({
-      success: true,
-      data: insuranceType,
-    });
-  } catch (error) {
     next(error);
   }
 };
-
-// @desc    Create insurance type
-// @route   POST /api/v1/insuranceTypes
-// @access  Private
-exports.addInsuranceType = async (req, res, next) => {
-  try {
-    const insuranceType = await InsuranceType.create(req.body);
-
-    res.status(201).json({
-      success: true,
-      data: insuranceType,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-
-// @desc   Update insurance type
-// @route  PUT /api/v1/insuranceTypes/:id
-// @access Private
-exports.updateInsuranceType = async (req, res, next) => {
-  try {
-    const insuranceType = await InsuranceType.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true, runValidators: true }
-    );
-
-    if (!insuranceType) {
-      return next(
-        new ErrorResponse(
-          `Unable to locate insurance type for ${req.params.id}`,
-          404
-        )
-      );
+  // @desc    Get a Insurance type
+  // @route   GET /api/v1/insuranceTypes/:id
+  // @access  Public
+  exports.getInsuranceType = async (req, res, next) => {
+    try {
+      const insuranceType = await InsuranceType.findById(req.params.id);
+      if (!insuranceType) {
+        return next(
+          new ErrorResponse(
+            `Unable to locate insurance type for ${req.params.id}`,
+            404
+          )
+        );
+      }
+      res.status(200).json({
+        success: true,
+        data: insuranceType,
+      });
+    } catch (error) {
+      next(error);
     }
-    res.status(200).json({
-      success: true,
-      data: insuranceType,
-      msg: `Update insurance type data for ${req.params.id}`,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+  };
 
-// @desc    Delete insurance type
-// @route   DELET /api/v1/insuranceTypes/:id
-// @access  Private
-exports.deleteInsuranceType = async (req, res, next) => {
-  try {
-    const insuranceType = await InsuranceType.findByIdAndDelete(req.params.id);
-    if (!insuranceType) {
-      return next(
-        new ErrorResponse(
-          `Unable to locate insurance type for ${req.params.id}`,
-          404
-        )
-      );
+  // @desc    Create insurance type
+  // @route   POST /api/v1/insuranceTypes
+  // @access  Private
+  exports.addInsuranceType = async (req, res, next) => {
+    try {
+      const insuranceType = await InsuranceType.create(req.body);
+
+      res.status(201).json({
+        success: true,
+        data: insuranceType,
+      });
+    } catch (error) {
+      next(error);
     }
-    res.status(200).json({
-      success: true,
-      data: insuranceType,
-      msg: `Delete insurance type data for ${req.params.id}`,
-    });
-  } catch (error) {
-    next(error);
-    /*  res.status(400).json({
+  };
+
+  // @desc   Update insurance type
+  // @route  PUT /api/v1/insuranceTypes/:id
+  // @access Private
+  exports.updateInsuranceType = async (req, res, next) => {
+    try {
+      const insuranceType = await InsuranceType.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true, runValidators: true }
+      );
+
+      if (!insuranceType) {
+        return next(
+          new ErrorResponse(
+            `Unable to locate insurance type for ${req.params.id}`,
+            404
+          )
+        );
+      }
+      res.status(200).json({
+        success: true,
+        data: insuranceType,
+        msg: `Update insurance type data for ${req.params.id}`,
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  // @desc    Delete insurance type
+  // @route   DELET /api/v1/insuranceTypes/:id
+  // @access  Private
+  exports.deleteInsuranceType = async (req, res, next) => {
+    try {
+      const insuranceType = await InsuranceType.findByIdAndDelete(
+        req.params.id
+      );
+      if (!insuranceType) {
+        return next(
+          new ErrorResponse(
+            `Unable to locate insurance type for ${req.params.id}`,
+            404
+          )
+        );
+      }
+      res.status(200).json({
+        success: true,
+        data: insuranceType,
+        msg: `Delete insurance type data for ${req.params.id}`,
+      });
+    } catch (error) {
+      next(error);
+      /*  res.status(400).json({
       success: false,
       msg: "Bad request",
     }); */
-  }
-};
+    }
+  };
+
--- middleware/error.js
+++ middleware/error.js
@@ -5,12 +5,24 @@ const errorHandler = (err, req, res, next) => {
   error.message = err.message;
 
   // log to console for the developer
-  console.log(err.stack.red);
+  console.log(err);
 
   // mongoose bad object ID
   if (err.name === "CastError") {
     const message = `Resource was not found with ID of ${err.value}`;
     error = new ErrorResponse(message, 404);
+  }
+
+  // mongoose dublicate key
+  if (err.code === 11000) {
+    const message = `Duplicate field value entered`;
+    error = new ErrorResponse(message, 400);
+  }
+
+  // mongoose validation error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
   }
 
   res.status(error.statusCode || 500).json({
